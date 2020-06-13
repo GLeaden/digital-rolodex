1d0a610045c162f8b4731cbe8e5d7a11afdd6405
G <goleaden@gmail.com>
2020-06-13 15:24:38 -0400

    added basic POST functionality - no db yet

--- src/main/java/com/rolodex/digitalrolodex/ContactsController.java
+++ src/main/java/com/rolodex/digitalrolodex/ContactsController.java
@@ -1,5 +1,8 @@
 package com.rolodex.digitalrolodex;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.rolodex.digitalrolodex.Contact;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,8 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 class ContactsController {
     @PostMapping("/contact")
-    Contact newContacts(@RequestBody String newContact) {
-
-        return newContact;
+    List<Contact> newContacts(@RequestBody String newContact) {
+        List<Contact> contacts = new ArrayList<Contact>();
+        String[] contactList = newContact.split("\\R");
+        for (String contact: contactList){
+            ContactParser parser = new ContactParser();
+            contacts.add(parser.parseContact(contact));
+        }
+        return contacts;
   }
 }

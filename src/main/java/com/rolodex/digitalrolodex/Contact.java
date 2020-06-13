705c378fb18d0009b7e3dd70f32902cc12560af9
G <goleaden@gmail.com>
2020-06-13 15:25:11 -0400

    messing around with date and names - added getters

--- src/main/java/com/rolodex/digitalrolodex/Contact.java
+++ src/main/java/com/rolodex/digitalrolodex/Contact.java
@@ -1,19 +1,25 @@
 package com.rolodex.digitalrolodex;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 public class Contact{
     /* 
-        A contact CAN have all of the below. A contact MUST have a name.
+        A contact CAN have all of the below. A contact MUST have an fName and lName.
     */
-    private String name; // required
+    private String fName;
+    private String mName;
+    private String lName;
     private String nickname;
     private String phoneNumber;
     private String email; 
-    private Date birthday;
+    private String birthday;
 
-    Contact(String name, String nickname, String phoneNumber, String email, Date birthday) {
-        this.name = name;
+    Contact(){}
+
+    Contact(String fName, String mName, String lName, String nickname, String phoneNumber, String email, String birthday) {
+        this.fName = fName;
+        this.mName = mName;
+        this.lName = lName;
         this.nickname = nickname;
         this.phoneNumber = phoneNumber;
         this.email = email;
@@ -21,9 +27,24 @@ public class Contact{
     }
 
     public String getName(){
-        return this.name;
+        return this.fName + " " + this.mName + " " + this.lName;
     }
 
+    public String getNickname(){
+        return this.nickname;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+    
+    public String getPhoneNumber(){
+        return this.phoneNumber;
+    }
+
+    public String getBirthday(){
+        return this.birthday;
+    }
 
 
 }
